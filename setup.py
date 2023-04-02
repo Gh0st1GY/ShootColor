a45e62fb9b43b35b48682db583391f65444a90cb
Gh0st{1GY} <129657410+Gh0st1GY@users.noreply.github.com>
2023-04-02 14:26:11 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='1color',
+    version='1.0.0',
+    description='A Python module for color effects',
+    author='Gh0st1GY',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
